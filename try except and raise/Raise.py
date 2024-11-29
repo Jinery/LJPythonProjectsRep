3a952366315aec2b869d1a81d274a1fb48a6e48f
Jinery <168351649+Jinery@users.noreply.github.com>
2024-11-29 19:03:14 +0000

    Update Raise.py

--- try except and raise/Raise.py
+++ try except and raise/Raise.py
@@ -3,13 +3,13 @@
 
 def divide(a, b):
     if b == 0:
-        raise ValueError("Деление на ноль недопустимо!") #Заменим наш ValueError на свою ошибку, в которой будет следующий текст: "Деление на ноль недопустимо!".
+        raise ZeroDivisionError("Деление на ноль недопустимо!") #Заменим наш ValueError на свою ошибку, в которой будет следующий текст: "Деление на ноль недопустимо!".
     return a / b
 
 try:
     result = divide(10, 0)
     print(result)
-except ValueError as e:
+except ZeroDivisionError as e:
     print(e)
 
 #Поскольку функции мы ещё не изучили, то по-быстрому объясню код.
